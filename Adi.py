dcc590312f96812662e54a8f4b67e53b66fbba2f
adityakudale4906 <147793413+adityakudale4906@users.noreply.github.com>
2023-10-13 10:56:28 +0530

    Add files via upload

---
+++ Adi.py
@@ -0,0 +1,10 @@
+class adi:
+    def __init__ (self):
+        self.name="adi"
+        self.age=8
+    def display (self):
+
+        print("name is",self.name)
+        print("age is",self.age)
+ob=adi()
+ob.display()
